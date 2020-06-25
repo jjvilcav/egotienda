9b2fff9a46f711113b2c836c6c1df065d0f26048
Joel <jjvilcav@uni.pe>
2020-06-24 22:52:30 -0500

    re re re actualizacion

--- productos/urls.py
+++ productos/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import url
 from django.urls import path
-from .views import VistasView, DetalladoView
+from .views import VistasView, DetalladoView, NuevaCategoriaView
 
 urlpatterns = [
     path('',VistasView.as_view(),name='index'),
     path('<slug:slug>', DetalladoView.as_view(),name='detallado'),
+    path('formulario/',NuevaCategoriaView.as_view(),name='formulario'),
 ]
 
--- productos/views.py
+++ productos/views.py
@@ -3,19 +3,14 @@ from django.shortcuts import render
 # Create your views here.
 from django.views.generic import TemplateView
 from .models import Categoria, Subcategoria, Producto
+from .forms import ProductoForm
 
 class VistasView(TemplateView):
     template_name = "productos/index.html"
     
     def get_context_data(self, **kwargs):
         kwargs['categoria'] = Categoria.objects.all()
-        return kwargs
-
-    def get_context_data(self, **kwargs):
         kwargs['subcategoria'] = Subcategoria.objects.all()
-        return kwargs
-
-    def get_context_data(self, **kwargs):
         kwargs['productos'] = Producto.objects.all()
         return kwargs
 
@@ -26,3 +21,18 @@ class DetalladoView(TemplateView):
         producto = kwargs.get('slug')
         kwargs['productos'] = Producto.objects.get(slug=producto)
         return kwargs
+
+class NuevaCategoriaView(TemplateView):
+    template_name = "productos/form.html"
+
+    def get_context_data(self, **kwargs):
+        kwargs['form'] = ProductoForm
+        return kwargs
+    
+    def post(self,request,*args,**kwargs):
+        context = self.get_context_data(**kwargs)
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+        context['form']=form
+        return self.render_to_response(context)
